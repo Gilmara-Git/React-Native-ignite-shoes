65555fd4b6ee6142ea56e04def06b45b7a89016c
Gilmara Pimentel <gilmarapq@hotmail.com>
2023-11-10 19:05:48 -0500

    feat: add OneSignal for Notifications

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -8,9 +9,19 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+import { tagUserEmailCreate , tagUserEmailDelete, tagsForDynamicMessages } from './src/notifications/notificationsTags';
+
+// Colocar isso em variavel de ambiente e adicionar no gitignore
+
+OneSignal.setAppId('31673c23-6859-4574-9e54-a942d1e712e6');
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  
+  tagUserEmailCreate('gilmara@gmail.com');
+  tagUserEmailDelete();
+  tagsForDynamicMessages('Gilmara', 'IgniteShoes', 'gilmara@gmail.com'); 
 
   return (
     <NativeBaseProvider theme={THEME}>
---
+++ src/notifications/notificationsTags.ts
@@ -0,0 +1,18 @@
+import OneSignal from "react-native-onesignal";
+
+export const tagUserEmailCreate = (email: string) =>{
+    OneSignal.sendTag('user_email', email)
+
+};
+
+export const tagUserEmailDelete = () =>{
+    OneSignal.deleteTag('user_email')
+};
+
+export const tagsForDynamicMessages = (userName: string, appName: string, userEmail: string)=>{
+    OneSignal.sendTags({
+        user_name: userName,
+        app_name: appName,
+        user_email: userEmail
+    })
+}
