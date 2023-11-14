bb345907a1c4941c1b0203c257fe919d755781b4
Gilmara Pimentel <gilmarapq@hotmail.com>
2023-11-14 09:02:32 -0500

    refactor: move the notification from App to routes

--- App.tsx
+++ App.tsx
@@ -8,21 +8,27 @@ import { Routes } from './src/routes';
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
+
 import { CartContextProvider } from './src/contexts/CartContext';
 import { tagUserEmailCreate , tagUserEmailDelete, tagsForDynamicMessages } from './src/notifications/notificationsTags';
 
 // Colocar isso em variavel de ambiente e adicionar no gitignore
 
 OneSignal.setAppId('31673c23-6859-4574-9e54-a942d1e712e6');
+OneSignal.promptForPushNotificationsWithUserResponse();
 
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   
   tagUserEmailCreate('gilmara@gmail.com');
   tagUserEmailDelete();
   tagsForDynamicMessages('Gilmara', 'IgniteShoes', 'gilmara@gmail.com'); 
 
+
+
+  
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar
@@ -30,9 +36,11 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
+
       <CartContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
       </CartContextProvider>
+
     </NativeBaseProvider>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,36 @@
+import { useEffect, useState} from 'react';
 import { useTheme } from 'native-base';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { Notification  } from '../components/Notification';
+import OneSignal,  { NotificationReceivedEvent, OSNotification} from 'react-native-onesignal';
 
 import { AppRoutes } from './app.routes';
 
 export function Routes() {
   const { colors } = useTheme();
+  const [ notification, setNotification ] = useState<OSNotification>();
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
 
+
+  useEffect(()=>{
+    const unsubscribe = OneSignal.setNotificationWillShowInForegroundHandler((notificationReceivedEvent: NotificationReceivedEvent)=>{
+      const notificationContent = notificationReceivedEvent.getNotification();
+      setNotification(notificationContent);
+    })
+    
+    return () => unsubscribe;
+
+  },[]);
+
   return (
     <NavigationContainer theme={theme}>
       <AppRoutes />
+      {
+      // notification?.title &&
+        <Notification title={'notification.title'}  onClose={()=>setNotification(undefined)}/>
+    }
     </NavigationContainer>
   );
 }
